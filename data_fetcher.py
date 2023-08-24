c4288cbe7a351f01ab63e854c6dc9607e6f64381
Onur Sevket Aslan <50531008+osevas@users.noreply.github.com>
2023-08-24 13:13:47 +0300

    final dataframe has been formed.

--- data_fetcher.py
+++ data_fetcher.py
@@ -5,21 +5,38 @@ Objective: creating object data_fetcher
 """
 # Libraries
 import yfinance as yf
+import pandas as pd
 
 
 class Data_fetcher:
     """
     object that will acquire data from Yahoo Finance
     """
-    def __init__(self, stock_names:str):
+    def __init__(self, stock_names:list):
         """Initializing an object with stock_names
 
         Args:
-            stock_names (string): names of the stocks
+            stock_names (list): list of names of the stocks
         """
         self.tickers = stock_names
     
-    def print_info(self):
-        ge = yf.Ticker(self.tickers)
-        print(ge.info)
+    def print_ticker_info(self):
+        """
+        Prints summary info of one ticker
+
+        Returns:
+            _type_: _description_
+        """
+        ticker = yf.Ticker(self.tickers[0])
+        print(ticker.info)
         return None
+    
+    def create_dataframe(self, start_date='2000-01-03', end_date='2013-05-31') -> pd.DataFrame:
+        """
+        Downloads ticker information and saves them in pandas dataframe
+
+        Returns:
+            pd.DataFrame: _description_
+        """
+        df = yf.download(self.tickers, start=start_date, end=end_date)
+        return df
--- main.py
+++ main.py
@@ -9,8 +9,35 @@ def main():
     """
     Main function of the code
     """
-    fetcher1 = Data_fetcher('GE')
-    fetcher1.print_info()
+    
+    # list_tickers = ['BAC', 'GE', 'JDSU', 'XOM', '^GSPC', 'DGS3MO', 'DGS1', 'DGS5', 'DGS10', 'DAAA', 'DBAA', 'DCOILWTICO']
+
+    list_tickers = ['GE', '^GSPC'] # GE stock, SP500
+    fetcher_ticker = Data_fetcher(list_tickers)
+
+    list_tres = ['^IRX'] # US Treasury 13-week bond rate
+    fetcher_tres = Data_fetcher(list_tres)
+   
+
+    # Downloading stocks data
+    # fetcher1.print_ticker_info()
+    data_tickers = fetcher_ticker.create_dataframe()
+    data_tickers_close = data_tickers.loc[:, 'Close'].copy()
+    data_tickers_close.rename(columns={'^GSPC':'SP500'}, inplace=True)
+    # print(data_tickers_close.head())
+
+    # Downloading bond rate data
+    # fetcher_tres.print_ticker_info()
+    data_tres = fetcher_tres.create_dataframe()
+    data_tres.rename(columns={'Close':'DGS3MO'}, inplace=True)
+    # print(data_tres.head())
+
+    # Joining two dataframes above
+    data_final = data_tickers_close.merge(data_tres['DGS3MO'], on='Date')
+
+    print(data_final.head())
+
+
 
 if __name__ == '__main__':
     main()
